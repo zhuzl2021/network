e60b9d05d339bb3be4b0bc4689c71a93a05703a1
zhuzl2021 <85337463+zhuzl2021@users.noreply.github.com>
2021-06-05 21:48:23 +0800

    Add files via upload

---
+++ network/display_interface-ethernet-brief_huawei/display_interface-ethernet-brief_huawei.py
@@ -0,0 +1,55 @@
+
+from netmiko import Netmiko
+from tabulate import tabulate
+import threading
+from textfsm import TextFSM
+from queue import Queue
+semaphore = threading.Semaphore(10)
+
+result_file = open('contain.txt', 'w')
+ip_list = open('ip_addresses')
+
+threads = []
+except_list = []
+
+
+def ssh_session(ip_list, output_q):
+    if semaphore.acquire():
+        try:
+            print(threading.current_thread().getName() + " got permissions")
+            device = {"device_type":'huawei_telnet', "ip": ip_list, "username": "itc",
+                      "password": "itc@1669", }
+            net_connect = Netmiko(**device)
+            # print(net_connect.find_prompt())
+            others = '\n' + 'hostip ' + ip_list + '\n' + net_connect.find_prompt() + '\n'
+            net_connect.send_command('screen-length 0 temporary')
+            command = 'dis int Ethernet brief | include 10M|100M'
+            output = others + net_connect.send_command(command, strip_command=False)
+            result_file.write(output)
+        # except TimeoutError:
+        #     cc = ('%s is unreachable' % ip_list)
+        #     except_list.append(cc)
+        except Exception as e:
+            dd = str(e) + ip_list
+            except_list.append(dd)
+        semaphore.release()
+        print(threading.current_thread().getName() + " released permissions")
+
+
+for ip in ip_list.readlines():
+    t1 = threading.Thread(target=ssh_session, args=(ip, Queue()))
+    t1.start()
+    threads.append(t1)
+for t2 in threads:
+    t2.join()
+result_file.close()
+result_list = []
+with open('contain.txt') as r, open('display_interface-ethernet-brief_hw_template') as f:
+    re_table = TextFSM(f)
+    header = re_table.header
+    result = r.read()
+    for i in re_table.ParseText(result):
+        result_list.append(i)
+print(tabulate(result_list, header))
+print('\n'.join(except_list))
+result_file.close()
